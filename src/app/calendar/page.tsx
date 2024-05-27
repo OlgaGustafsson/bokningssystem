65fc13e91436edfee6c8311f8f489d333fc69ce0
Olga Gustafsson <olgagustafsson@MacBook-Air-som-tillhor-Olga.local>
2024-05-27 19:49:57 +0200

    clean up calendar

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -10,7 +10,6 @@ import { today } from "@/utils/today";
 import { getFutureDate } from "@/utils/futureDate";
 
 export default function Calendar() {
-  //const [selectedOption, setSelectedOption] = useState("");
   const [selectedDate, setSelectedDate] = useState("");
   const [selectedRoom, setSelectedRoom] = useState("");
   const [roomName, setRoomName] = useState("");
@@ -20,31 +19,25 @@ export default function Calendar() {
 
   const futureDate = getFutureDate();
 
-
   useEffect(() => {
-    fetchRooms().then(data => {
-      setRooms(data);
-    }).catch(error => {
-      console.error("Error fetching rooms:", error);
-    });
+    fetchRooms()
+      .then((data) => {
+        setRooms(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching rooms:", error);
+      });
   }, []);
 
-  
-
-  // useEffect(() => {
-  //   const today = new Date().toISOString().split("T")[0];
-  //   setSelectedValue(today);
-  // }, []);
-
   // välj datum i kalender
-  const handleValueChange = (event: { target: { value: any; }; }) => {
+  const handleValueChange = (event: { target: { value: any } }) => {
     const value = event.target.value;
     setSelectedDate(value);
     localStorage.setItem("selectedDate", value);
   };
 
   // välj rum
-  const handleRoomChange = (event: { target: { value: any; }; }) => {
+  const handleRoomChange = (event: { target: { value: any } }) => {
     const room = event.target.value;
     setSelectedRoom(room);
     localStorage.setItem("selectedRoom", room);
@@ -53,12 +46,13 @@ export default function Calendar() {
 
   // room_name
   const selectedRoomId = parseInt(selectedRoom, 10);
-  const selectedRoomData = rooms.find(room => room.room_id === selectedRoomId);
-  const selectedRoomName =  selectedRoomData ? selectedRoomData.room_name : '';
+  const selectedRoomData = rooms.find(
+    (room) => room.room_id === selectedRoomId
+  );
+  const selectedRoomName = selectedRoomData ? selectedRoomData.room_name : "";
 
   const handleSubmit = async (event: { preventDefault: () => void }) => {
     event.preventDefault();
-  
 
     if (!selectedDate) {
       alert("Vänligen välj ett datum");
@@ -78,22 +72,16 @@ export default function Calendar() {
     localStorage.setItem("selectedRoomName", selectedRoomName);
     setRoomName(selectedRoomName);
     setSelectedDate(selectedDate);
-    
+
     // setSelectedDate("");
     // setSelectedRoom("");
-    //setSelectedOption("");
     //localStorage.clear();
 
     console.log(selectedDate);
     console.log(typeof selectedDate);
     console.log("Value of selectedDate in Calendar:", selectedDate);
   };
-  // console.log(selectedValue);
-  // console.log(selectedRoom);
 
-  //const today = new Date().toISOString().split("T")[0];
-
-  
   return (
     <>
       <div
@@ -103,26 +91,27 @@ export default function Calendar() {
         <div className="flex flex-col w-auto pl-2 pr-2 gap-4 text-4xl text-stone-400">
           <p className="font-semibold">Boka rum</p>
 
-          <form className="flex flex-col gap-4 xl:flex-col" onSubmit={handleSubmit}>
+          <form
+            className="flex flex-col gap-4 xl:flex-col"
+            onSubmit={handleSubmit}
+          >
             <div className="flex flex-col gap-4">
-
               <div
                 className="flex flex-col text-xl gap-3 
                   border rounded-md border-solid-stone-600 h-12 w-64"
               >
-
-                  <input
-                    key={selectedDate}
-                    type="date"
-                    className="text-xl w-60 h-12"
-                    value={selectedDate}
-                    onChange={handleValueChange}
-                    min={today}
-                    max={futureDate}
-                  />
+                <input
+                  key={selectedDate}
+                  type="date"
+                  className="text-xl w-60 h-12"
+                  value={selectedDate}
+                  onChange={handleValueChange}
+                  min={today}
+                  max={futureDate}
+                />
               </div>
             </div>
-            
+
             <div className="flex flex-col gap-4">
               <div
                 className="flex flex-col text-xl gap-3 pb-8
@@ -135,33 +124,22 @@ export default function Calendar() {
                     onChange={handleRoomChange}
                   >
                     <option value="">Välj rum</option>
-    
-                    {rooms.length > 0 && 
-                    rooms.map((room) => (
-                      <option key={room.room_id} value={room.room_id}>
-                        {room.room_name}
-                      </option>
-                    ))}
 
-                    {/* <option value="stora rummet">Stora rummet</option>
-                    <option value="röda rummet">Röda rummet</option>
-                    <option value="gröna rummet">Gröna rummet</option>
-                    <option value="bruna rummet">Bruna rummet</option>
-                    <option value="blå rummet">Blå rummet</option> */}
+                    {rooms.length > 0 &&
+                      rooms.map((room) => (
+                        <option key={room.room_id} value={room.room_id}>
+                          {room.room_name}
+                        </option>
+                      ))}
                   </select>
                 </label>
               </div>
-
-              
             </div>
 
             <div className="flex flex-col gap-4">
               <div className="flex flex-col">
                 <button
                   type="submit"
-                  // className="flex justify-center items-center text-md w-40 h-12
-                  // rounded-3xl border border-stone-500 bg-white hover:bg-stone-100
-                  // text-stone-500"
                   className="flex justify-center items-center text-xl w-32 h-12 
                     rounded-xl border border-stone-900 bg-white hover:bg-stone-100
                     text-stone-500"
@@ -174,14 +152,11 @@ export default function Calendar() {
         </div>
 
         <div className="flex flex-col w-auto p-3">
-          {/* <div className="">Här kan du boka:</div> */}
-          
-          {showBookingTable && <BookingTable roomName={roomName} selectedDate={selectedDate} />}
-
+          {showBookingTable && (
+            <BookingTable roomName={roomName} selectedDate={selectedDate} />
+          )}
         </div>
-
       </div>
-
     </>
   );
 }
