de2cb5abcad620633a98ab823058f66c2e53720e
Olga Gustafsson <olgagustafsson@MacBook-Air-som-tillhor-Olga.local>
2024-05-28 21:11:40 +0200

    styling login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,7 @@
 // modal login
 
 import { useEffect, useState } from "react";
+import { GrClose } from "react-icons/gr";
 
 interface LoginProps {
   isOpen: boolean;
@@ -35,20 +36,23 @@ const Login: React.FC<LoginProps> = ({ isOpen, onClose, onLogin}) => {
   return (
     <>
       {isOpen && (
-        <div className="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 flex justify-center items-center">
+        <div className="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 
+                        flex justify-center items-center">
           <div className="bg-white p-4 rounded-lg w-72 flex flex-col">
-            <div className="flex justify-end">
+            <div className="flex justify-end pb-8">
               <button
                 onClick={handleClose}
-                className="bg-stone-200 text-gray-700 px-4 py-2 
-                rounded-md hover:bg-gray-400 flex justify-center max-w-16"
+                className="bg-stone-200 text-gray-700 px-2 py-2 
+                          rounded-md hover:bg-gray-400 flex justify-center 
+                          max-w-16 text-2xl"
               >
-                Stäng
+                {/* Stäng */}
+                <GrClose />
               </button>
             </div>
-            <h2 className="text-lg font-bold mb-4 text-center">Logga in</h2>
+            <h2 className="text-3xl font-bold mb-4 text-center text-stone-400">Logga in</h2>
             <form onSubmit={handleSubmit}
-              className="pb-4"
+              className="p-8"
             >
               <div className="mb-4">
                 {/* <label
@@ -60,12 +64,14 @@ const Login: React.FC<LoginProps> = ({ isOpen, onClose, onLogin}) => {
                 <input
                   type="email"
                   placeholder="E-post"
-                  className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+                  className="mt-1 block w-full rounded-md border-gray-300 
+                            shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 
+                            focus:ring-opacity-50 h-12"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
-              <div className="mb-4">
+              <div className="mb-8">
                 {/* <label
                   htmlFor="password"
                   className="block text-sm font-medium text-gray-700"
@@ -75,15 +81,17 @@ const Login: React.FC<LoginProps> = ({ isOpen, onClose, onLogin}) => {
                 <input
                   type="password"
                   placeholder="Lösenord"
-                  className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+                  className="mt-1 block w-full rounded-md border-gray-300 
+                            shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 
+                            focus:ring-opacity-50 h-12"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
               <button
                 type="submit"
-                className="bg-blue-500 text-white px-4 py-2 rounded-md 
-                hover:bg-blue-600"
+                className="bg-blue-500 text-white text-xl px-8 py-4 
+                          rounded-md hover:bg-blue-600"
               >
                 Logga in
               </button>
