1a1e3a7fe5cd5dee2ef0029abd6759caaec620fe
Olga Gustafsson <olgagustafsson@MacBook-Air-som-tillhor-Olga.local>
2024-06-08 18:13:10 +0200

    clean up BookingTable

--- src/components/BookingTable.tsx
+++ src/components/BookingTable.tsx
@@ -20,7 +20,6 @@ export default function BookingTable({
   const [selectedTime, setSelectedTime] = useState("");
   const [times, setTimes] = useState<Time[]>([]);
   const [comment, setComment] = useState("");
-  //const [bookingStatus, setBookingStatus] = useState(false);
   const [booking, setBooking] = useState(null);
   const [bookings, setBookings] = useState([]);
   const [games, setGames] = useState<Game[]>([]);
@@ -66,19 +65,19 @@ export default function BookingTable({
     localStorage.setItem("isOpen", String(status));
   };
 
-  const handleMaxPlayersChange = (event) => {
+  const handleMaxPlayersChange = (event: { target: { value: any; }; }) => {
     const maxPlayersValue = event.target.value;
     setMaxPlayers(maxPlayersValue);
     localStorage.setItem("maxPlayers", maxPlayersValue);
   };
 
-  const handleTimeChange = (event) => {
+  const handleTimeChange = (event: { target: { value: any; }; }) => {
     const time = event.target.value;
     setSelectedTime(time);
     localStorage.setItem("selectedTime", time);
   };
 
-  const handleCommentChange = (event) => {
+  const handleCommentChange = (event: { target: { value: any; }; }) => {
     const commentValue = event.target.value;
     setComment(commentValue);
     localStorage.setItem("comment", commentValue);
@@ -127,8 +126,7 @@ export default function BookingTable({
     setMaxPlayers("");
     setSelectedTime("");
     setComment("");
-    // setBookingStatus(false);
-
+    
     // if (newBooking) 
     router.push("/mypage");
    
@@ -244,117 +242,9 @@ export default function BookingTable({
           </button>
         </div>
 
-
       </div>
 
       {/* update */}
-
-
-      {/* <div className="overflow-x-auto w-min-screen">
-        <table className="table border border-solid-stone-500 w-full">
-          <thead>
-            <tr>
-              <th className="border border-solid-stone-500 text-left">Datum</th>
-              <th className="border border-solid-stone-500 text-left">
-                Kategori
-              </th>
-              <th className="border border-solid-stone-500 text-left">Öppen</th>
-              <th className="border border-solid-stone-500 text-left">
-                Deltagare
-              </th>
-              <th className="border border-solid-stone-500 text-left">Tid</th>
-              <th className="border border-solid-stone-500 text-left">
-                Kommentar
-              </th>
-            </tr>
-          </thead>
-          <tbody className="border border-solid-stone-500">
-            <tr>
-              <td className="border border-solid-stone-500 h-12">
-                {selectedDate}
-              </td>
-              <td className="border border-solid-stone-500">
-                <select
-                  value={selectedCategory}
-                  onChange={handleCategoryChange}
-                >
-                  <option value="">Välj kategori</option>
-      
-                  {games.length > 0 &&
-                    games.map((game) => (
-                      <option key={game.game_id} value={game.game_id}>
-                        {game.game_category}
-                      </option>
-                    ))}
-                </select>
-              </td>
-              <td className="border border-solid-stone-500">
-                <select
-                  value={isOpen.toString()}
-                  onChange={handleOpenStatusChange}
-                >
-                  <option value="">Välj</option>
-                  <option value="true">Ja</option>
-                  <option value="false">Nej</option>
-                </select>
-              </td>
-              <td className="border border-solid-stone-500">
-                {isOpen && (
-                  <select value={maxPlayers} onChange={handleMaxPlayersChange}>
-                    <option value=""> Välj antal deltagare</option>
-                    <option value="5">Max 5</option>
-                    <option value="10">Max 10</option>
-                    <option value="15">Max 15</option>
-                  </select>
-                )}
-              </td>
-              <td className="border border-solid-stone-500">
-                <select value={selectedTime} onChange={handleTimeChange}>
-                  <option value="">Välj tid</option>
-
-                  {times.map((time) => (
-                    <option key={time.time_id} value={time.time_id}>
-                      {time.time}
-                    </option>
-                  ))}
-                </select>
-              </td>
-              <td className="border border-solid-stone-500">
-                <textarea
-                  className=""
-                  placeholder="Skriv kommentar"
-                  value={comment}
-                  onChange={handleCommentChange}
-                  maxLength={100}
-                />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-
-      <div className="flex flex-row justify-center gap-3 pt-8">
-        <Link href="/">
-          <button
-            className="flex justify-center items-center text-xl w-32 h-12 
-              rounded-xl border border-stone-900 bg-white hover:bg-stone-100
-              text-stone-500"
-          >
-            Tillbaka
-          </button>
-        </Link>
-
-        <button
-          className="flex justify-center items-center text-xl w-32 h-12 
-              rounded-xl border border-stone-500 bg-stone-500 hover:bg-stone-100
-              text-white"
-          type="submit"
-          onClick={handleSaveBooking}
-          // disabled={!isFormVald} // inaktivera knappen om formuläret inte är giltigt
-        >
-          Spara
-        </button>
-      </div> */}
 
     </div>
   );
