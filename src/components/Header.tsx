96fbae76267f36522af30387f74961d5cdb983b2
Olga Gustafsson <olgagustafsson@MacBook-Air-som-tillhor-Olga.local>
2024-06-08 16:26:42 +0200

    clean up Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,37 +3,17 @@
 import React, { useState } from 'react'
 import Link from 'next/link'
 import Navbar from './Navbar'
-import { FaHome } from "react-icons/fa";
-import { useRouter } from 'next/navigation';
+//import { FaHome } from "react-icons/fa";
 
 export default function Header() {
-
-
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
-  //   const router = useRouter();
-
-  //   const handleToggleLogin = () => {
-  //       if(isLoggedIn) {
-  //           setIsLoggedIn(false);
-  //           router.push('/');
-  //       } else {
-  //           setIsLoggedIn(true);
-  //           router.push('/calendar');
-  //       }
-  //   };
-
-
 
   return (
     <header 
         className="sticky top-0 flex flex-wrap items-start pt-6 
         justify-around h-18 bg-white z-[1000]"
-        // className=" fixed bottom-0 w-full flex flex-wrap items-center pt-6
-        // justify-around h-18 bg-stone-300"
     >
         <Link href="/"> 
           <div className="flex flex-wrap justify-center text-stone-500 text-2xl">
-            {/* <h1 className="text-xl pb-4">Logo</h1> */}
 
             {/* <FaHome /> */}
 
@@ -57,18 +37,6 @@ export default function Header() {
           <Navbar />
 
         </div>
-
-        {/* <div className='flex flex-wrap justify-center pt-6 text-stone-500'>
-        <button onClick={handleToggleLogin}
-                className={`${
-                  isLoggedIn
-                    ? "text-blue-400"
-                    : "text-orange-300"
-                }`}
-        >
-          {isLoggedIn ? 'Logga ut' : 'Logga in'}
-        </button>
-        </div> */}
     </header>
   )
 }
